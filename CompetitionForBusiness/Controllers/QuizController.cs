2ad84b7398744a842bd835c39e172f3a0561ddc5
Hemithea <seydanuraydin50@gmail.com>
2026-08-25 06:48:00 +0300

    Add RegisterDto class for user registration

--- CompetitionForBusiness/Controllers/QuizController.cs
+++ CompetitionForBusiness/Controllers/QuizController.cs
@@ -12,6 +12,14 @@ using System;
 
 namespace CompetitionForBusiness.Controllers
 {
+public class RegisterDto
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? FullName { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+    }
  
         
         [ApiController]
